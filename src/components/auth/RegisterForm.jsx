49cac77559eb9c13e0818f63d92089f985f0c42c
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-22 17:27:46 +0600

    implemented register functionality

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -1,21 +1,56 @@
 "use client";
+import { postUser } from '@/actions/server/auth';
+import { showAlert } from '@/utils/showAlert';
 import React from 'react';
 
 const RegisterForm = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const casePattern = /^(?=.*[a-z])(?=.*[A-Z]).+$/;
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = {
+            nid: form.nid.value,
+            name: form.name.value,
+            email: form.email.value,
+            password: form.password.value,
+        };
+        if (!formData.nid ||
+            !formData.name ||
+            !formData.email ||
+            !formData.password
+        ) {
+            return showAlert("Error", "Please fill up all fields", "error");
+        }
+        else if (!emailPattern.test(formData.email)) {
+            return showAlert("Error", "Invalid email", "error");
+        }
+        else if (formData.password.length < 6) {
+            return showAlert("Error", "Password must be at least 6 characters", "error");
+        }
+        else if (!casePattern.test(formData.password)) {
+            return showAlert("Error", "Password must contain at least one uppercase and one lowercase letter", "error");
+        }
+        const result = await postUser(formData);
+        if (!result.success) {
+            return showAlert("Error", result.message, "error");
+        }
+        showAlert("Success", result.message, "success");
+    };
     return (
         <div className="min-h-[calc(100vh-76px)] flex justify-center items-center p-6">
             <div className="card bg-base-200 w-full max-w-sm">
                 <div className="card-body">
                     <h2 className="text-3xl text-center font-bold">Register</h2>
-                    <form className="fieldset">
+                    <form onSubmit={handleSubmit} className="fieldset">
                         <label className="label">NID No</label>
-                        <input type="text" className="input w-full" placeholder="NID No" />
+                        <input type="text" name="nid" className="input w-full" placeholder="NID No" />
                         <label className="label">Name</label>
-                        <input type="text" className="input w-full" placeholder="Name" />
+                        <input type="text" name="name" className="input w-full" placeholder="Name" />
                         <label className="label">Email</label>
-                        <input type="email" className="input w-full" placeholder="Email" />
+                        <input type="text" name="email" className="input w-full" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" className="input w-full" placeholder="Password" />
+                        <input type="password" name="password" className="input w-full" placeholder="Password" />
                         <button className="btn btn-info mt-4">Register</button>
                     </form>
                 </div>
---
+++ src/lib/authOptions.js
@@ -0,0 +1,60 @@
+import { dbConnect } from "./dbConnect";
+import bcrypt from "bcryptjs";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        CredentialsProvider({
+            // Sign in with {name} button
+            name: 'Email and Password',
+            // Form inputs
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "Enter Email" },
+                password: { label: "Password", type: "password", placeholder: "Enter password" },
+            },
+            async authorize(credentials, req) {
+                const {email, password} = credentials;
+                const user = await dbConnect("users").findOne({email});
+                if(!user) return null;
+                const isPasswordOk = await bcrypt.compare(password, user.password);
+                if(isPasswordOk) return user;
+                return null;
+            }
+        }),
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }){
+            try{
+                const payload = {
+                    ...user,
+                    provider: account.provider,
+                    providerId: account.providerAccountId,
+                    createdAt: new Date().toISOString()
+                };
+                if(!user?.email){
+                    return false;
+                }
+                const isExist = await dbConnect("users").findOne({
+                    email: user.email,
+                });
+                if(!isExist){
+                    const result = await dbConnect("users").insertOne(payload);
+                }
+                return true;
+            }
+            catch(error){
+                return false;
+            }
+        },
+        async session({ session, token, user }){
+            return session;
+        },
+        async jwt({ token, user, account, profile, isNewUser }){
+            if(user){
+                token.email = user.email;
+            }
+            return token;
+        }
+    }
+}
