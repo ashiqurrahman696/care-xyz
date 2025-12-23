7c0b0acd4d383a168dbbf5ed321fc63f1f6e6fc9
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-23 19:20:08 +0600

    updated postBooking method

--- src/actions/server/booking.js
+++ src/actions/server/booking.js
@@ -1,8 +1,12 @@
 "use server";
 
+import { authOptions } from "@/lib/authOptions";
 import { dbConnect } from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
 
 export const postBooking = async(payload) => {
+    const {user} = (await getServerSession(authOptions)) || {};
+    if (!user) return {success: false};
     const newBooking = {...payload};
     const result = await dbConnect("bookings").insertOne(newBooking);
     if(result.acknowledged){
